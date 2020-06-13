79e2cb206a8bc3565cb0c794e90f6a5273a93c09
LuigiImVector <luigi.vector15@virgilio.it>
2020-06-13 16:18:24 +0200

    Update login page

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
         $email = $_POST['email'];
         $password = $_POST['password'];
         $confirmPassword = $_POST['confirm-password'];
-
+        /* $checkbox = $_POST['checkbox']; */
+      
+        /* Analisi dati ricevuti */
         if (!$_POST['email'])
         {
             $error .= "Inserire una EMail</br>";
@@ -21,6 +23,17 @@
         if ($confirmPassword == "")
         {
             $error .= "Inserire la conferma della password</br>"; /* Fa schifo come frase -- Da cambiare */
+        }
+
+        if ($password != $confirmPassword)
+        {
+            $error .= "Le password non sono uguali</br>"; /* Frase da migliorare */
+        }
+
+        /* Inizio sessione */
+        if ($error == "")
+        {
+
         }
     }
     
