630bb909eecd0b8a4d7811f6417c791a1d5658ec
LuigiImVector <luigi.vector15@virgilio.it>
2020-06-14 21:19:35 +0200

    Update login page

--- index.php
+++ index.php
@@ -37,13 +37,13 @@
             $error .= "Le password non sono uguali</br>"; /* Frase da migliorare */
         }
 
-        $query = "SELECT `email` FROM gymDatabase WHERE id='"".mysqli_real_escape_string($db, $email).""'";
+        /* $query = "SELECT `email` FROM gymDatabase WHERE email='".mysqli_real_escape_string($db, $email)."'";
         $result = mysqli_query($db, $query);
 
         if (mysqli_num_rows($result)>0)
         {
-            $error .= "L\'EMail inserita è già stata utilizzata, inserirne un\'altra.</br>"
-        }
+            $error .= "L\'EMail inserita è già stata utilizzata, inserirne un\'altra.</br>";
+        } */
 
 /* Validazione password */
         if (strlen($password) < 12)
@@ -54,22 +54,31 @@
             $error .= "La password deve avere meno di 256 caratteri."; /* Non sono sicuro che ci sia un limite di lunghezza */
         } else if (!preg_match("#[0-9]+#", $password))
         {
-            $error .= "La password deve contenere almeno ..."; /* Ultimare */
+            $error .= "La password deve contenere almeno un numero.";
         } else if (!preg_match("#[A-Z]+#", $password))
         {
-            $error .= "La password deve contenere almeno ..."; /* Ultimare */
+            $error .= "La password deve contenere almeno un carattere maiuscolo.";
         } else if (!preg_match("#[a-z]+#", $password))
         {
-            $error .= "La password deve contenere almeno ..."; /* Ultimare */
-        } else if(!preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $password)) /* Ultimare */
+            $error .= "La password deve contenere almeno un carattere minuscolo.";
+        } /* else if (!preg_match("/!\"$%&/\=?'^_-.;,:@#*+[]/", $password))
         {
-            $error .= "La password deve contenere almeno ..."; /* Ultimare */
-        }
+            $error .= "La password deve contenere almeno un carattere speciale.";
+        } */
 
         /* Inizio sessione */
         if ($error == "")
         {
-            
+            $query = "INSERT INTO gymDatabase(email) VALUES ('".mysqli_real_escape_string($db, $email)."') LIMIT 1";
+            mysqli_query($db, $query);
+
+            $query = "SELECT id FROM gymDatabase WHERE email='".mysqli_real_escape_string($db, $email)."'";
+            $result = mysqli_query($db, $query);
+            /* $row = mysqli_fetch_assoc($result); */
+            $salt = $row['$result'];
+            echo $result;
+
+            $password = hash('sha256', $password . $salt);
         }
     }
     
