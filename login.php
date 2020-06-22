2d41bcbb26e341b4ad87e6246f5406736af30291
LuigiImVector <luigi.vector15@virgilio.it>
2020-06-22 19:49:59 +0200

    Login page [I]

--- login.php
+++ login.php
@@ -16,10 +16,10 @@ if(isset($_POST['submit']))
         $error .= "Inserire una password.</br>";
     }
 
-    $query = "SELECT `password` FROM `gymUsers` WHERE email='".mysqli_real_escape_string($db, $email)."'";
+    $query = "SELECT `password` FROM `gymUsers` WHERE email=('".mysqli_real_escape_string($db, $email)."')";
     $result = mysqli_query($db, $query);
     $row = mysqli_fetch_assoc($result);
-    $databasePassword = $row['id'];
+    $databasePassword = $row['password'];
 
     if (mysqli_num_rows($result)==0)
     {
@@ -33,20 +33,13 @@ if(isset($_POST['submit']))
 /* Inizio sessione */
     if ($error == "")
     {
-        $query = "INSERT INTO gymUsers (email, password) VALUES ('".mysqli_real_escape_string($db, $email)."', '')";
-        mysqli_query($db, $query);
-        
-        $query = "SELECT id FROM gymUsers WHERE email=('$email')";
+        $query = "SELECT id FROM gymUsers WHERE email=('".mysqli_real_escape_string($db, $email)."')";
         $result = mysqli_query($db, $query);
         $row = mysqli_fetch_assoc($result);
-        $salt = $row['id'];
+        $userID = $row['id'];
         /*
         echo $salt;     -- Serve per testare
         */
-
-        $hashedPassword = hash('sha256', $salt . $password . hash('sha256', $salt));
-        $query = "UPDATE gymUsers SET password='".mysqli_real_escape_string($db, $hashedPassword)."' WHERE email='".mysqli_real_escape_string($db, $email)."'";
-        mysqli_query($db, $query);
 
         session_start();    /* L'ID serve? */
         if ($checkbox == "1")
