9a8636304fa24e7e504d2c2ed8cff040d33d7350
LuigiImVector <luigi.vector15@virgilio.it>
2020-08-13 15:59:11 +0200

    Aggiunta PreTimer e audio 3sec. Pulsante stop funzionante.

--- timer.js
+++ timer.js
@@ -2,8 +2,11 @@ document.getElementById("timer-submit").onclick = function()
 {
     var timeValue = document.getElementById("time").value;
     var repeatValue = document.getElementById("repeat").value;
+    var threeSec = new Audio('audio/countdown-3sec.mp3');
     var x = 0;
     var y = 0;
+    var counterPreTimer = 3;
+    var preTimerCheck = false;
     var error = false;
 
     if (timeValue == "")
@@ -23,22 +26,72 @@ document.getElementById("timer-submit").onclick = function()
         document.getElementById("popup-timer").style.display = "block";
 
         /* Timer */
-        
+      
         var interval = setInterval(function () {
 
-            x++;
-            document.getElementById("popup-number").innerHTML = x;
-
-            if(x>timeValue)
+            /* PreTimer */
+            if (preTimerCheck == false)
             {
-                x=0;
-                document.getElementById("popup-number").innerHTML = x;
-                y++;
+                if (counterPreTimer==3)
+                {
+                    threeSec.play();
+                }
+               
+                document.getElementById("popup-number").innerHTML = counterPreTimer;
+                
+                
+
+                if (counterPreTimer==0)
+                {
+                    /* counterPreTimer=0; */
+                    preTimerCheck = true;
+                    /* document.getElementById("popup-number").innertHTML = counterPreTimer; */
+                }
+
+                counterPreTimer--;
             }
 
-            if (y==repeatValue) {
-                clearInterval(interval);
+
+            /* Timer effettivo */
+            else if (preTimerCheck == true)
+            {
+                
+                if(x==(timeValue-3))
+                {
+                    threeSec.play();
+                }
+
+                document.getElementById("popup-number").innerHTML = x;
+
+                if(x>timeValue)
+                {
+                    x=0;
+                    document.getElementById("popup-number").innerHTML = x;
+                    y++;
+                }
+
+                if (y==repeatValue)
+                {                
+                    clearInterval(interval);
+                }
+                x++;
             }
+
+
+            document.getElementById("popup-stop").onclick = function ()
+            {
+                document.getElementById("popup-timer").style.display = "none";
+
+                clearInterval(interval);
+                threeSec.pause();
+                threeSec.currentTime = 0;
+
+                document.getElementById("popup-number").innerHTML = 3;
+                counterPreTimer=3;
+                x=0;
+                y=0;
+            }
+
         }, 1000);
 
 
@@ -48,7 +101,3 @@ document.getElementById("timer-submit").onclick = function()
 
 }
 
-document.getElementById("popup-stop").onclick = function ()
-{
-    document.getElementById("popup-timer").style.display = "none";
-}
